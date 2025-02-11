6951dd8407b98fa2c1f70eec51ff041ce49783f5
ahmedsobhinext <ahmedsobhinext@gmail.com>
2025-02-11 18:23:01 +0200

    done

--- src/pages/components/EventCard.js
+++ src/pages/components/EventCard.js
@@ -2,19 +2,19 @@ import Link from 'next/link';
 
 
 // components/EventCard.js
-export default function EventCard({ event}) {
+export default function EventCard({ eventt}) {
   return (
-    <Link href={`/events/${event.id}`}>
+    <Link href={`/events/${eventt.id}`}>
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
       <img 
-        src={event.image} 
-        alt={event.name} 
+        src={eventt.image} 
+        alt={eventt.name} 
         className="w-full h-48 object-cover"
       />
       <div className="p-4">
-        <h3 className="text-xl font-semibold mb-2">{event.name}</h3>
-        <p className="text-gray-600 mb-1">Date: {event.date}</p>
-        <p className="text-gray-600">Location: {event.location}</p>
+        <h3 className="text-xl font-semibold mb-2">{eventt.name}</h3>
+        <p className="text-gray-600 mb-1">Date: {eventt.date}</p>
+        <p className="text-gray-600">Location: {eventt.location}</p>
         <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
 View Details
       </button>
