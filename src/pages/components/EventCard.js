1405e12bb941da0bba141cbb2e7459a0c98f97ac
ahmedsobhinext <ahmedsobhinext@gmail.com>
2025-02-11 18:26:43 +0200

    done1

--- src/pages/components/EventCard.js
+++ src/pages/components/EventCard.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 // components/EventCard.js
 export default function EventCard({ eventt}) {
   return (
-    <Link href={`/events/${eventt.id}`}>
+    <Link href={`/events/${eventt.idd}`}>
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
       <img 
         src={eventt.image} 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,21 @@ export default function Home() {
   // Mock data for featured events with images
   const featuredEvents = [
     { 
-      id: 1, 
+      idd: 1, 
       name: 'Concert Night', 
       date: '2024-01-15', 
       location: 'Riyadh',
       image: 'https://images.unsplash.com/photo-1506157786151-b8491531f063' 
     },
     { 
-      id: 2, 
+      idd: 2, 
       name: 'Amusement Park Day', 
       date: '2024-02-20', 
       location: 'Jeddah',
       image: 'https://www.ey.com/adobe/dynamicmedia/deliver/dm-aid--4e157450-6d65-4f5e-910a-0f7ca39c5b97/ey-fun-roller-coaster.jpg?preferwebp=true&width=1600&quality=85'
     },
     { 
-      id: 3, 
+      idd: 3, 
       name: 'Sports Championship', 
       date: '2024-03-10', 
       location: 'Dammam',
@@ -57,7 +57,7 @@ export default function Home() {
         <h2 className="text-2xl font-semibold mt-8">Featured Events</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
           {featuredEvents.map((event) => (
-            <EventCard key={event.id} event={event} />
+            <EventCard key={event.idd} event={event} />
           ))}
         </div>
       </div>
